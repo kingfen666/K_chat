82bce9153a124c88a4b4d0b59dcfe96a3eaea05c
King <2647405352@qq.com>
2023-12-02 13:29:26 +0800

    密码进行md5加密，改正拦截器路径

--- src/main/java/org/kingfen/k_chat/Controller/login.java
+++ src/main/java/org/kingfen/k_chat/Controller/login.java
@@ -6,10 +6,12 @@ import org.kingfen.k_chat.Bean.User;
 import org.kingfen.k_chat.KChatApplication;
 import org.kingfen.k_chat.sql.UserMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import static org.kingfen.k_chat.KChatApplication.userMap;
@@ -21,8 +23,8 @@ public class login {
     @ResponseBody
     public String pwd_login(String username,String password){
         System.out.println(username);
-        System.out.println(password);
-        User login = userMap.login(username, password);
+        System.out.println(DigestUtils.md5DigestAsHex(password.getBytes(StandardCharsets.UTF_8)));
+        User login = userMap.login(username, DigestUtils.md5DigestAsHex(password.getBytes(StandardCharsets.UTF_8)));
         if (login!=null){
             return JSON.toJSONString(login);
         }
